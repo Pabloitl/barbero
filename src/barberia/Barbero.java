9af84bf9f1bc7482d2098c8e807fd859f3803936
Marcela Mosiño <68758494+MarcelaMosino@users.noreply.github.com>
2020-09-23 12:49:39 -0500

    Documentación

--- src/barberia/Barbero.java
+++ src/barberia/Barbero.java
@@ -1,26 +1,36 @@
+/**
+   * Clase que simula al barbero, cuando está ocupado,
+   * al realizar el corte de pelo, atender y despachar.
+   * Clase que utiliza semáforos
+**/
+
 package barberia;
 
 import java.io.PrintStream;
 import java.util.concurrent.Semaphore;
 
-public class Barbero {
-    public static int PUEDE_ATENDER = 2;
+public class Barbero {    
+    //Refiere al estado del barbero
+    public static int PUEDE_ATENDER = 2; 
+    //Semáforo que indica si puede atender
+    private final Semaphore cupo; 
     
-    private final Semaphore cupo;
-    
+    //Constructor que inicializa el semáforo
     public Barbero() {
         cupo = new Semaphore(PUEDE_ATENDER);
     }
     
+    //Método que simula el corte de pelo
     public void atiende(Cliente cliente) throws InterruptedException {
-        while (!cupo.tryAcquire())
-            Thread.sleep(100);
+        while (!cupo.tryAcquire()) // Mientras no esté ocupado, puede atender
+            Thread.sleep(100);     // Tiempo en que realiza el corte
         cliente.printEnters(System.out);
         Thread.sleep(cliente.tiempoEnAccion);
         cliente.printExits(System.out);
-        cupo.release();
+        cupo.release();  // Se despacha al cliente
     }
     
+    // Método que nos indica que el barbero está durmiendo
     public void sleep(PrintStream out) {
         out.println("Barbero duerme");
     }
